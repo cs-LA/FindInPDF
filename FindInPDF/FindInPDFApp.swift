9f408f7defb0e1239e4739011e2570d3eb2701ea
Clemens Schützdeller <Cl.Schuetzdeller@icloud.com>
2022-10-26 17:35:05 +0200

    Initial Commit

---
+++ FindInPDF/FindInPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindInPDFApp.swift
+//  FindInPDF
+//
+//  Created by Clemens Schützdeller on 26.10.22.
+//
+
+import SwiftUI
+
+@main
+struct FindInPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
